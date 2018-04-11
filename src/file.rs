e9fe963950aef5bb3082e659ea219f315173cacb
Martin Indra <martin.indra@mgn.cz>
2018-04-11 19:12:28 +0000

    Use String instead of &str in Err
    This needs to be done so errors with constructed string (without 'static
    lifetime) can be used (moved by return).

--- src/file.rs
+++ src/file.rs
@@ -3,16 +3,17 @@ use std::fs::{OpenOptions, create_dir};
 use std::io::Write;
 use word::Word;
 
-pub fn add_one(word: &Word) -> Result<(), &'static str> {
+pub fn add_one(word: &Word) -> Result<(), String> {
     let mut file_path = match env::home_dir() {
         Some(path) => path,
-        None => return Err("Couldn't locate home directory.")
+        None => return Err("Couldn't locate home directory.".to_string())
     };
     file_path.push(".vole");
 
     if !file_path.exists() {
         if let Err(_) = create_dir(&file_path) {
-            return Err("Couldn't create .vole directory in home directory.");
+            return Err("Couldn't create .vole directory in home directory."
+                       .to_string());
         }
     }
 
@@ -22,12 +23,12 @@ pub fn add_one(word: &Word) -> Result<(), &'static str> {
     open_options.append(true).create(true);
     let mut file = match open_options.open(file_path) {
         Ok(file) => file,
-        Err(_) => return Err("Couldn't open dictionary file.")
+        Err(_) => return Err("Couldn't open dictionary file.".to_string())
     };
 
     let line = word.serialize();
     if let Err(_) = file.write_all(line.as_bytes()) {
-        return Err("Couldn't write to dictionary file.")
+        return Err("Couldn't write to dictionary file.".to_string())
     }
 
     Ok(())
