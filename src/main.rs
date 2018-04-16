4d44c00659cdcb31b5229bcaea74792d4d7d73b7
Martin Indra <martin.indra@mgn.cz>
2018-04-16 20:40:16 +0000

    Implement better arg parsing with clap crate

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,47 @@
+extern crate clap;
 extern crate rand;
 extern crate vole;
 
-use std::env;
-use std::iter::Iterator;
+use clap::{Arg, ArgMatches, App, SubCommand};
 use std::process;
 use vole::{file, card, learn};
 
 fn main() {
-    let mut args = env::args().skip(1);
+    let mut app = App::new("VoLe").version("0.1.0")
+        .author("Martin Indra <martin.indra@mgn.cz>")
+        .about("CLI for flashcard learning");
 
-    let cmd = next_arg(&mut args);
+    let question_arg = Arg::with_name("question").required(true);
+    let answer_arg = Arg::with_name("answer").required(true);
+    let add_sub_command = SubCommand::with_name("add")
+        .about("Stores a new flashcard.")
+        .arg(question_arg).arg(answer_arg);
+    app = app.subcommand(add_sub_command);
 
-    if let Err(report) = execute(&cmd, args) {
-        eprint!("{}\n", report);
+    let learn_sub_command = SubCommand::with_name("learn")
+        .about("Starts question and answer learning loop.");
+    app = app.subcommand(learn_sub_command);
+
+    let matches = app.get_matches();
+    if let Err(report) = execute(matches) {
+        eprintln!("{}", report);
         process::exit(1);
     }
 }
 
-fn execute<T>(cmd: &str, mut args: T) -> Result<(), String>
-where
-    T: Iterator<Item = String>,
-{
-    if cmd == "add" {
-        let value_a: String = next_arg(&mut args);
-        let value_b: String = next_arg(&mut args);
-
-        let card = card::Card::with_random_id(value_a, value_b);
+fn execute(matches: ArgMatches) -> Result<(), String> {
+    if let Some(matches) = matches.subcommand_matches("add") {
+        let question = matches.value_of("question").unwrap().to_string();
+        let answer = matches.value_of("answer").unwrap().to_string();
+        let card = card::Card::with_random_id(question, answer);
         file::write_one(&card)?;
-    } else if cmd == "learn" {
+        return Ok(());
+    }
+
+    if let Some(_) = matches.subcommand_matches("learn") {
         learn::learning_loop()?;
-    } else {
-        let err = format!("Unrecognized argument: {}", cmd);
-        return Err(err);
+        return Ok(());
     }
 
-    Ok(())
-}
-
-fn next_arg<T: Iterator<Item = String>>(args: &mut T) -> String {
-    match args.next() {
-        Some(arg) => arg,
-        None => {
-            eprint!(
-                "Invalid arguments, try: \
-                 vole add <variant-a> <variant-b>\n"
-            );
-            process::exit(1);
-        }
-    }
+    panic!("Unrecognized command.")
 }
