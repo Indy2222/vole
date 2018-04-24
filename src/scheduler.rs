5960f5d82b6c0bb350e618bccaf0f7db9ac199f2
Martin Indra <martin.indra@mgn.cz>
2018-04-24 22:24:20 +0000

    Add GPL3 copyright notice to every source file

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,8 +1,26 @@
+// Source Code, Copyright (C) 2018  Martin Indra
+// Algorithm SM-2, (C) Copyright SuperMemo World, 1991.
+//
+// This file is part of VoLe.
+//
+// VoLe is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+// SuperMemo 2 Algorithm
+//
 // This file contains re-implementation of SuperMemo 2 algorithm. See
 // http://supermemopedia.com/wiki/Licensing_SuperMemo_Algorithm
 // for information about SM-2 algorithm licensing
-//
-// Algorithm SM-2, (C) Copyright SuperMemo World, 1991.
 //
 //    http://www.supermemo.com
 //    http://www.supermemo.eu
