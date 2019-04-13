6d965b58726b152562100c571761819ccce4a2cd
Martin Indra <martin.indra@mgn.cz>
2019-04-13 18:14:29 +0200

    Add some Card unittests

--- src/card.rs
+++ src/card.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2018  Martin Indra
+// Copyright (C) 2018, 2019  Martin Indra
 //
 // This file is part of VoLe.
 //
@@ -23,7 +23,7 @@ pub struct Card {
 
 impl Card {
     pub fn parse_id(id: &str) -> Result<u64, String> {
-        u64::from_str_radix(id, 16).map_err(|r| format!("Failed to parse ID: {}", r))
+        u64::from_str_radix(id, 16).map_err(|r| format!("Failed to parse card ID: {}", r))
     }
 
     pub fn serialize_id(id: u64) -> String {
@@ -48,5 +48,39 @@ impl Card {
 
     pub fn answer(&self) -> &str {
         &self.answer
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse_id() {
+        assert_eq![
+            Card::parse_id("B000000000000001").unwrap(),
+            12682136550675316737
+        ];
+        assert_eq![
+            Card::parse_id("b000000000000001").unwrap(),
+            12682136550675316737
+        ];
+        assert_eq![
+            Card::parse_id("xxx").err().unwrap(),
+            String::from("Failed to parse card ID: invalid digit found in string")
+        ];
+    }
+
+    #[test]
+    fn test_serialize_id() {
+        assert_eq![Card::serialize_id(49154), String::from("000000000000c002")];
+    }
+
+    #[test]
+    fn test_card() {
+        let card = Card::new(123, String::from("What?"), String::from("Something!"));
+        assert_eq!(card.id(), 123);
+        assert_eq!(card.question(), "What?");
+        assert_eq!(card.answer(), "Something!");
     }
 }
